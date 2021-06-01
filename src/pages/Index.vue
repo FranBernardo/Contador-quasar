c06b71d53345de5f1226d4f6d4cf99b58c0ca779
FranBernardo <francielebernardo88@hotmail.com>
2021-06-01 16:50:10 -0300

    projeto solo em quasar

---
+++ src/pages/Index.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="row column">
+      <h2>Contador</h2>
+      <h1 class="flex flex-center">{{ contador }}</h1>
+      <div class="flex flex-center">
+        <q-btn
+          push
+          color="white"
+          class="q-mr-md"
+          text-color="primary"
+          label="-"
+          @click="less"
+        />
+        <q-btn
+          push
+          color="white"
+          class="q-mr-md"
+          text-color="primary"
+          label="zero"
+          @click="zero"
+        />
+        <q-btn
+          push
+          color="white"
+          class="q-mr-md"
+          text-color="primary"
+          label="+"
+          @click="sum"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    sum() {
+      this.contador += 1;
+    },
+    zero() {
+      this.contador = 0;
+    },
+    less() {
+      this.contador -= 1;
+    },
+  },
+};
+</script>
